d947fe7ed044686e807a44f53ea05dcf1a0d8c44
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-08-03 23:30:56 +0530

    components: Navbar setup

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        Reading List
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
