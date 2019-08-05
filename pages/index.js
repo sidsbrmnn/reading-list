d6e7cd1de2068a7b52158b6c25bfeb18f14ac79e
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-08-05 22:23:17 +0530

    pages: index updated with BookList

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,63 @@
-import axios from "axios";
-import React from "react";
+import React, { Component } from "react";
 
-import Form from "./../components/Form";
+import BookList from "../components/BookList";
+import NewBook from "../components/NewBook";
 import Page from "../components/Page";
+import { addBook, getBooks, deleteBook } from "../utils/books";
 
-export default class extends React.Component {
-  static async getInitialProps({ req }) {
-    const { data: list } = await axios.get("/api/books");
+export default class extends Component {
+  state = { books: [], error: "" };
 
-    return { list };
+  componentDidMount() {
+    this.updateBooks();
   }
 
-  render() {
-    console.log(this.props.list);
+  displayError = () => {
+    const error = this.state.error;
+    if (error)
+      return (
+        <div className="alert alert-danger mb-0" role="alert">
+          {error}
+        </div>
+      );
+  };
 
+  updateBooks = async () => {
+    const books = await getBooks();
+    this.setState({ books });
+  };
+
+  doSubmit = async book => {
+    const error = await addBook(book);
+    if (error) {
+      this.setState({ error });
+      return error;
+    }
+
+    this.updateBooks();
+  };
+
+  handleDelete = async book => {
+    const error = await deleteBook(book._id);
+    if (error) return this.setState({ error });
+    this.updateBooks();
+  };
+
+  render() {
     return (
       <Page title="Reading List">
         <section className="container py-5">
           <h1>New Book</h1>
-          <Form />
+          <NewBook onSubmit={this.doSubmit} />
+        </section>
+        <section>
+          <div className="col-lg-4 col-md-6 col-sm-8 col-10 mx-auto">
+            {this.displayError()}
+          </div>
+        </section>
+        <section className="container py-5">
+          <h1>Book List</h1>
+          <BookList data={this.state.books} onDelete={this.handleDelete} />
         </section>
       </Page>
     );
