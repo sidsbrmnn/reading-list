6228680e5ee3465892a3e3c3ee07f8c5bc8c2374
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-10-08 22:03:07 +0530

    Use axios.create to set baseURL
    Signed-off-by: Siddharth Subramanian <sidsbrmnn@gmail.com>

--- utils/books.js
+++ utils/books.js
@@ -1,24 +1,28 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000/api/books';
+const instance = axios.create({ baseURL: 'http://localhost:3000/api/books' });
 
 export async function addBook(book) {
     try {
-        await axios.post('/', book);
+        await instance.post('/', book);
     } catch (ex) {
-        if (ex.response.status !== 200) return ex.response.data;
+        if (ex.response.status !== 200) {
+            return ex.response.data;
+        }
     }
 }
 
 export async function getBooks() {
-    const { data } = await axios.get('/');
+    const { data } = await instance.get('/');
     return data;
 }
 
 export async function deleteBook(id) {
     try {
-        await axios.delete('/' + id);
+        await instance.delete('/' + id);
     } catch (ex) {
-        if (ex.response.status !== 200) return ex.response.data;
+        if (ex.response.status !== 200) {
+            return ex.response.data;
+        }
     }
 }
