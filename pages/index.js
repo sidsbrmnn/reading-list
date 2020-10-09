9cd40d8b0120ceefe737d17a0f4fc24988c7cc17
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-10-09 23:54:10 +0530

    Update homepage
    - Use functional component
    - Remove Page layout
    
    Signed-off-by: Siddharth Subramanian <sidsbrmnn@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,68 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import BookList from '../components/BookList';
 import NewBook from '../components/NewBook';
-import Page from '../components/Page';
 import { addBook, getBooks, deleteBook } from '../utils/books';
 
-export default class extends Component {
-    state = { books: [], error: '' };
+const HomePage = () => {
+    const [books, setBooks] = useState([]);
 
-    componentDidMount() {
-        this.updateBooks();
+    async function updateBooks() {
+        const data = await getBooks();
+        setBooks(data);
     }
 
-    displayError = () => {
-        const error = this.state.error;
-        if (error)
-            return (
-                <div className='alert alert-danger mb-0' role='alert'>
-                    {error}
-                </div>
-            );
-    };
+    useEffect(() => {
+        updateBooks();
+    }, []);
 
-    updateBooks = async () => {
-        const books = await getBooks();
-        this.setState({ books });
-    };
+    // displayError = () => {
+    //     const error = this.state.error;
+    //     if (error)
+    //         return (
+    //             <div className="mb-0 alert alert-danger" role="alert">
+    //                 {error}
+    //             </div>
+    //         );
+    // };
 
-    doSubmit = async book => {
+    async function doSubmit(book) {
         const error = await addBook(book);
         if (error) {
-            this.setState({ error });
-            return error;
+            return;
         }
 
-        this.updateBooks();
-    };
-
-    handleDelete = async book => {
-        const error = await deleteBook(book._id);
-        if (error) return this.setState({ error });
-        this.updateBooks();
-    };
-
-    render() {
-        return (
-            <Page title='Reading List'>
-                <section className='container py-5'>
-                    <h1>New Book</h1>
-                    <NewBook onSubmit={this.doSubmit} />
-                </section>
-                <section>
-                    <div className='col-lg-4 col-md-6 col-sm-8 col-10 mx-auto'>
-                        {this.displayError()}
-                    </div>
-                </section>
-                <section className='container py-5'>
-                    <h1>Book List</h1>
-                    <BookList
-                        data={this.state.books}
-                        onDelete={this.handleDelete}
-                    />
-                </section>
-            </Page>
-        );
+        updateBooks();
     }
-}
+
+    async function handleDelete(book) {
+        const error = await deleteBook(book._id);
+        if (error) {
+            return;
+        }
+
+        updateBooks();
+    }
+
+    return (
+        <>
+            <section className="py-5">
+                <h1>New Book</h1>
+                <NewBook onSubmit={doSubmit} />
+            </section>
+
+            <section>
+                <div className="mx-auto col-lg-4 col-md-6 col-sm-8 col-10">
+                    {/* {this.displayError()} */}
+                </div>
+            </section>
+
+            <section className="py-5">
+                <h1>Book List</h1>
+                <BookList data={books} onDelete={handleDelete} />
+            </section>
+        </>
+    );
+};
+
+export default HomePage;
