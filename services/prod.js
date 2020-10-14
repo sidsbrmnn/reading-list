0c9b368fa85825e39b0617fb92496bb4449327ae
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-10-14 14:11:42 +0530

    Remove unnoticed express services xP lol
    Signed-off-by: Siddharth Subramanian <sidsbrmnn@gmail.com>

--- services/prod.js
+++
@@ -1,9 +0,0 @@
-const compression = require('compression');
-const helmet = require('helmet');
-const logger = require('morgan');
-
-module.exports = function (server) {
-    server.use(compression());
-    server.use(helmet());
-    server.use(logger('dev'));
-};
--- services/routes.js
+++
@@ -1,7 +0,0 @@
-const bodyParser = require('body-parser');
-
-module.exports = function (server) {
-    server.use(bodyParser.json());
-    server.use(bodyParser.urlencoded({ extended: false }));
-    server.use('/api/books', require('../routes/books'));
-};
